76b7ca90503ad9fd393c3ff0b5ee5ae56b1b08ad
wanglady <1843827089@qq.com>
2018-12-05 13:47:22 +0800

    resolve the element-ui icon can not show

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,7 +56,11 @@ module.exports = {
       },
       {
         test: /.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'file-loader'
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
@@ -64,14 +68,6 @@ module.exports = {
         options: {
           limit: 10000,
           name: utils.assetsPath('media/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|woff|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
     ]
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view v-transition/>
+    <router-view/>
   </div>
 </template>
 
